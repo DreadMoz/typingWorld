b071d5ead9dd35467e12dfddefd286c52920e4cf
WINDOWS10\rochy2moo <rochy2moo@gmail.com>
2023-07-24 01:06:37 +0900

    Fontsづまりからのリプレイス

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,136 +7,33 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    public GameObject inventry;
-    public GameObject equipment;
-    public GameObject ranking;
+    public GameManager gameMaster;
     public GameObject status;
     public Fade fade;
-    private Rigidbody rigidBody;
     private Animator animator;
     private NavMeshAgent agent;
     private float speed = 8f;
     private float inputHorizontal;
     private float inputVertical;
-    public bool enableInventry = false;
-    public bool enableRanking = false;
-    public bool enableStatus = false;
-    private int div = 10;
-    private int cutCount = 99;
 
-// Start is called before the first frame update
-void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();           // クリック箇所への移動用
         agent.speed = speed;
-        animator = GetComponent<Animator>();
-        animator.SetInteger("anim", 1);
-        animator.SetTrigger("Hi");
+
+        animator = GetComponent<Animator>();            // Playerのアニメーション
+        animator.SetInteger("anim", 1);                 // オープニングシーン0、ワールドシーン1
+        animator.SetTrigger("Hi");                      // アニメーションHi実行
     }
 
     // Update is called once per frame
     void Update()
     {
+        // フェードイン完了でなければフェードイン実行
         if (!fade.IsFadeInComplete()){
             transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
             return;
-        }
-        if (Input.GetKeyDown(KeyCode.I) && (cutCount >= div))
-        {
-            if (!enableStatus)
-            {
-                enableStatus = true;
-                enableInventry = true;
-            }
-            else
-            {
-                if (enableInventry)
-                {
-                    enableStatus = false;
-                    enableInventry = false;
-                }
-                else if (enableRanking)
-                {
-                    enableStatus = true;
-                    enableInventry = true;
-                    enableRanking = false;
-                }
-            }
-            cutCount = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.R) && (cutCount >= div))
-        {
-            if (!enableStatus)
-            {
-                enableStatus = true;
-                enableRanking = true;
-            }
-            else
-            {
-                if (enableRanking)
-                {
-                    enableStatus = false;
-                    enableRanking = false;
-                }
-                else if (enableInventry)
-                {
-                    enableStatus = true;
-                    enableRanking = true;
-                    enableInventry = false;
-                }
-            }
-            cutCount = 0;
-        }
-
-        if (cutCount < div)
-        {
-            if (cutCount < div / 2)
-            {
-                if (status.activeSelf)
-                {
-                    status.transform.position += new Vector3(0, 30, 0);
-                }
-                if ((inventry.activeSelf) && (!enableInventry))
-                {
-                    inventry.transform.position += new Vector3(110, 0, 0);
-                    equipment.transform.position += new Vector3(0, -35, 0);
-                }
-                if ((ranking.activeSelf) && (!enableRanking))
-                {
-                    ranking.transform.position += new Vector3(110, 0, 0);
-                }
-                cutCount++;
-
-                if (cutCount == div / 2)
-                {
-                    status.SetActive(false);
-                    inventry.SetActive(false);
-                    equipment.SetActive(false);
-                    ranking.SetActive(false);
-                }
-            }
-            else
-            {
-                if (enableStatus)
-                {
-                    status.SetActive(true);
-                    status.transform.position += new Vector3(0, -30, 0);
-                }
-                if (enableInventry)
-                {
-                    inventry.SetActive(true);
-                    equipment.SetActive(true);
-                    inventry.transform.position += new Vector3(-110, 0, 0);
-                    equipment.transform.position += new Vector3(0, 35, 0);
-                }
-                if (enableRanking)
-                {
-                    ranking.SetActive(true);
-                    ranking.transform.position += new Vector3(-110, 0, 0);
-                }
-                cutCount++;
-            }
         }
 
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Damage") || animator.GetCurrentAnimatorStateInfo(0).IsName("Hi"))
