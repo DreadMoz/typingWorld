12662f97c8b8f33cd3c6cae812f4d6c48e20911b
WINDOWS10\rochy2moo <rochy2moo@gmail.com>
2023-08-06 02:16:48 +0900

    すべてついか

---
+++ Assets/Script/Hand.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    private Item grabbingItem;
+    void Update()
+    {
+        this.transform.position = Input.mousePosition;
+    }
+
+    public Item GetGrabbingItem()
+    {
+        Item oldItem = grabbingItem;
+        grabbingItem = null;
+        return oldItem;
+    }
+
+    public void SetGrabbingItem(Item item)
+    {
+        grabbingItem = item;
+    }
+
+    public bool IsHavingItem()
+    {
+        return grabbingItem != null;
+    }
+}
