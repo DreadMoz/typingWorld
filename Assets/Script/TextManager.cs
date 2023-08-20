9daba7b9f28316ee2c3d722348a0e32eb6d5ad32
DreadMoz <rochy2moo@gmail.com>
2023-08-21 01:17:11 +0900

    Auth、Get、Set、DummyGet追加

--- Assets/Script/TextManager.cs
+++ Assets/Script/TextManager.cs
@@ -1,33 +1,12 @@
 using UnityEngine;
-using System.Runtime.InteropServices;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class TextManager : MonoBehaviour
 {
-    [DllImport("__Internal")]
-    private static extern void Firebase();
-
     public Text textInstance;
-    public RawImage imageInstance;
 
-    // js������X�V�����������ɌĂяo�����֐�
     public void UpdateText(string newText)
     {
-        Debug.Log("UpdateText called with newText: " + newText);
-        // �e�L�X�g�R���|�[�l���g�̎擾
-        textInstance.text = "zz";
-    }
-
-
-    void Start()
-    {
-        //js���̊֐����Ăяo���ăf�[�^�̊Ď��J�n
-    }
-
-    public void OnButtonX()
-    {
-        // �e�L�X�g�R���|�[�l���g�̎擾
-        textInstance.text = "button";
+        textInstance.text = newText;
     }
 }
